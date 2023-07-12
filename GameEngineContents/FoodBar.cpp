c4dbad2382bd8c58e65860c4adcbcad9b8e75a7d
Minseok Kim <mskim6800@naver.com>
2023-07-12 21:43:52 +0900

    김민석 : FoodUI 업데이트

--- GameEngineContents/FoodBar.cpp
+++ GameEngineContents/FoodBar.cpp
@@ -1,10 +1,11 @@
 #include "PrecompileHeader.h"
 #include "FoodBar.h"
-
+#include "Player.h"
 #include <GameEngineCore/GameEngineLevel.h>
 #include <GameEngineCore/GameEngineCamera.h>
 #include <GameEnginePlatform/GameEngineInput.h>
 #include <GameEngineCore/GameEngineUIRenderer.h>
+#include "UIFontRender.h"
 
 FoodBar::FoodBar() 
 {
@@ -26,10 +27,38 @@ void FoodBar::Start()
 	PlayerFoodUI->SetTexture("PlayerFoodUI.png");
 	PlayerFoodUI->GetTransform()->SetLocalPosition(PlayerFoodUIPos);
 	PlayerFoodUI->GetTransform()->SetLocalScale(PlayerFoodUIScale);
-
+	FontSet();
 }
 
 void FoodBar::Update(float _Delta)
 {
+	CurSatityRender->SetText(std::to_string(Player::MainPlayer->GetData().GetSatiety()));
+
+}
+
+void FoodBar::FontSet()
+{
+	CurSatityRender = CreateComponent<UIFontRender>(90);
+	CurSatityRender->SetFont(font);
+	CurSatityRender->SetFontFlag(FW1_CENTER);
+	CurSatityRender->SetScale(15);
+	CurSatityRender->SetColor(float4::White);
+	CurSatityRender->SetText("/");
+	CurSatityRender->GetTransform()->SetLocalPosition({ -555.0f,-310.0f,0.0f });
+
+	CurSatityRender = CreateComponent<UIFontRender>(90);
+	CurSatityRender->SetFont(font);
+	CurSatityRender->SetFontFlag(FW1_CENTER);
+	CurSatityRender->SetScale(24);
+	CurSatityRender->SetColor(float4::White);
+	CurSatityRender->GetTransform()->SetLocalPosition({ -575.0f,-305.0f,0.0f });
+
+	MaxSatityRender = CreateComponent<UIFontRender>(90);
+	MaxSatityRender->SetFont(font);
+	MaxSatityRender->SetFontFlag(FW1_CENTER);
+	MaxSatityRender->SetScale(24);
+	MaxSatityRender->SetColor(float4::White);
+	MaxSatityRender->SetText("100");
+	MaxSatityRender->GetTransform()->SetLocalPosition({ -530.0f,-305.0f,0.0f }); 
 }
 
--- GameEngineContents/FoodBar.h
+++ GameEngineContents/FoodBar.h
@@ -19,10 +19,17 @@ protected:
 	void Start();
 	void Update(float _Delta) override;
 private:
+	void FontSet();
 	std::shared_ptr<class GameEngineUIRenderer> PlayerFoodUI;
 
 	float4 PlayerFoodUIScale = { 34.0f,28.0f,0.0f };
 	float4 PlayerFoodUIPos = { -610.0f,-320.0f,0.0f };
+
+	std::shared_ptr<class UIFontRender> CurSatityRender;
+	std::shared_ptr<class UIFontRender> SlashRender;
+	std::shared_ptr<class UIFontRender> MaxSatityRender;
+	std::string font = "Aa카시오페아";
+
 
 };
 
